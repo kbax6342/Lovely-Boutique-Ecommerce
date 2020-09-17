d170308637da52bb641f18cb8d529dc32ce83576
kbax6342 <k3vin.baxt3r@gmail.com>
2020-09-16 22:49:03 -0400

    finished footer-social media icons-payments icons-copyight

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
+const shopify = require('shopify-buy')
 
 const app = express()
 const port = process.env.PORT || 3000;
@@ -10,6 +11,7 @@ const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialPath = path.join(__dirname, "../templates/partials")
+const productId = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0Lzc4NTc5ODkzODQ=';
 
 
 app.set('view engine', 'hbs')
@@ -26,6 +28,24 @@ app.use(express.static('views/images'))
 
 app.get('', (req, res) => {
     res.render('index', {
+        title: 'Lovely Larea Boutique'
+    })
+})
+
+app.get('/shop', (req, res) => {
+    res.render('shop', {
+        title: 'Lovely Larea Boutique'
+    })
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact', {
+        title: 'Lovely Larea Boutique'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
         title: 'Lovely Larea Boutique'
     })
 })
